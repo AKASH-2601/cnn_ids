2433301417639e2cfe8b9ba842e5c88f82a9ecfd
AKASH-2601 <akash26242931@gmail.com>
2025-03-23 22:17:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,15 +10,34 @@ st.markdown(
     """
     <style>
     div.stFormSubmitButton button {
-        background-color: #ff5733 !important;
-        color: white !important;
-        text-align: center !important;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 200px;
-        margin: auto;
+  cursor: pointer;
+  outline: 0;
+  display: inline-block;
+  font-weight: 400;
+  line-height: 1.5;
+  text-align: center;
+  background-color: transparent;
+  border: 1px solid transparent;
+  padding: 6px 12px;
+  font-size: 1rem;
+  border-radius: .25rem;
+  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+  color: #0d6efd;
+  border-color: #0d6efd;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 300px;
+    margin: auto;
     }
+
+div.stFormSubmitButton button:hover {
+      color: #fff;
+      background-color: #0d6efd;
+      border-color: #0d6efd;
+  }
+  
+
     </style>
     """,
     unsafe_allow_html=True
