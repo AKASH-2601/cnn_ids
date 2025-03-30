f61abac74fc9535b9757c0cb3b4d6bd092fd6efc
AKASH-2601 <akash26242931@gmail.com>
2025-03-30 12:35:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -268,6 +268,77 @@ def user_input_features():
 
         return input_data, submit_button
 
+def verify_input(data):
+        for key, value in data.items():
+            if value is None or value == "":
+                return False
+        return True
+
+    # Load the trained model
+try:
+    model = keras.models.load_model("my_cnn_model.keras")
+except Exception as e:
+    st.error(f"Error loading model: {e}")
+    st.stop()
+
+
+def preprocess_data(input_df):
+    selected_columns = [
+        'count', 'src_bytes', 'service', 'dst_bytes', 'dst_host_same_src_port_rate',
+        'srv_count', 'logged_in', 'dst_host_count', 'protocol_type',
+        'dst_host_srv_diff_host_rate', 'same_srv_rate', 'flag'
+    ]
+    input_df = pd.DataFrame([input_df])[selected_columns]
+    categorical_columns = ['protocol_type', 'flag']
+    all_categories = ['protocol_type_icmp', 'protocol_type_tcp', 'protocol_type_udp',
+                      'flag_OTH', 'flag_REJ', 'flag_RSTO', 'flag_RSTOS0', 'flag_RSTR',
+                      'flag_S0', 'flag_S1', 'flag_S2', 'flag_S3', 'flag_SF', 'flag_SH']
+    # Perform one-hot encoding for categorical features
+    input_df = pd.get_dummies(input_df, columns=categorical_columns)
+    # Ensure all one-hot encoded categories exist in the dataframe
+    for col in all_categories:
+        if col not in input_df.columns:
+            input_df[col] = 0  # Add missing columns with value 0
+    service_map = {
+        'http': 22, 'smtp': 50, 'finger': 17, 'domain_u': 11, 'auth': 3, 'telnet': 56,
+        'ftp': 18, 'eco_i': 13, 'ntp_u': 39, 'ecr_i': 14, 'other': 40, 'private': 45,
+        'pop_3': 43, 'ftp_data': 19, 'rje': 48, 'time': 59, 'mtp': 31, 'link': 29,
+        'remote_job': 47, 'gopher': 20, 'ssh': 52, 'name': 32, 'whois': 65, 'domain': 10,
+        'login': 30, 'imap4': 24, 'daytime': 8, 'ctf': 7, 'nntp': 38, 'shell': 49,
+        'IRC': 0, 'nnsp': 37, 'http_443': 23, 'exec': 16, 'printer': 44, 'efs': 15,
+        'courier': 5, 'uucp': 62, 'klogin': 26, 'kshell': 27, 'echo': 12, 'discard': 9,
+        'systat': 55, 'supdup': 54, 'iso_tsap': 25, 'hostnames': 21, 'csnet_ns': 6,
+        'pop_2': 42, 'sunrpc': 53, 'uucp_path': 63, 'netbios_ns': 34, 'netbios_ssn': 35,
+        'netbios_dgm': 33, 'sql_net': 51, 'vmnet': 64, 'bgp': 4, 'Z39_50': 2, 'ldap': 28,
+        'netstat': 36, 'urh_i': 60, 'X11': 1, 'urp_i': 61, 'pm_dump': 41, 'tftp_u': 57,
+        'tim_i': 58, 'red_i': 46
+    }
+    input_df['service'] = input_df['service'].map(service_map)
+    scaler = joblib.load('scalar_model.joblib')
+    num_cols = [
+        "count", "src_bytes", "dst_bytes", "dst_host_same_src_port_rate",
+        "srv_count", "dst_host_count", "dst_host_srv_diff_host_rate", "same_srv_rate"
+    ]
+    input_df[num_cols] = scaler.transform(input_df[num_cols])
+    return input_df
+
+def reset_form():
+    st.session_state.reset = True
+    st.session_state.protocol = 'tcp'
+    st.session_state.service = 'http'
+    st.session_state.flag = 'SF'
+    for key in ['count', 'src_bytes', 'dst_bytes', 'dst_host_same_src_port_rate',
+                'srv_count', 'logged_in', 'dst_host_count', 'dst_host_srv_diff_host_rate', 'same_srv_rate']:
+        st.session_state[key] = 0.00
+        
+def predict_model(input_df):
+    try:
+        prediction = model.predict(input_df)[0]
+        return "Anomaly" if prediction > 0.5 else "Normal"
+    except Exception as e:
+        return None
+
+
 def main_page():
     input_data, submit = user_input_features()
     if submit:
@@ -290,81 +361,7 @@ def main_page():
             st.session_state.username = ""
             st.rerun()
 
-    def verify_input(data):
-        for key, value in data.items():
-            if value is None or value == "":
-                return False
-        return True
-
-    # Load the trained model
-    try:
-        model = keras.models.load_model("my_cnn_model.keras")
-    except Exception as e:
-        st.error(f"Error loading model: {e}")
-        st.stop()
-
-    def preprocess_data(input_df):
-        selected_columns = [
-            'count', 'src_bytes', 'service', 'dst_bytes', 'dst_host_same_src_port_rate',
-            'srv_count', 'logged_in', 'dst_host_count', 'protocol_type',
-            'dst_host_srv_diff_host_rate', 'same_srv_rate', 'flag'
-        ]
-        input_df = pd.DataFrame([input_df])[selected_columns]
-
-        categorical_columns = ['protocol_type', 'flag']
-        all_categories = ['protocol_type_icmp', 'protocol_type_tcp', 'protocol_type_udp',
-                          'flag_OTH', 'flag_REJ', 'flag_RSTO', 'flag_RSTOS0', 'flag_RSTR',
-                          'flag_S0', 'flag_S1', 'flag_S2', 'flag_S3', 'flag_SF', 'flag_SH']
-
-        # Perform one-hot encoding for categorical features
-        input_df = pd.get_dummies(input_df, columns=categorical_columns)
-
-        # Ensure all one-hot encoded categories exist in the dataframe
-        for col in all_categories:
-            if col not in input_df.columns:
-                input_df[col] = 0  # Add missing columns with value 0
-
-        service_map = {
-            'http': 22, 'smtp': 50, 'finger': 17, 'domain_u': 11, 'auth': 3, 'telnet': 56,
-            'ftp': 18, 'eco_i': 13, 'ntp_u': 39, 'ecr_i': 14, 'other': 40, 'private': 45,
-            'pop_3': 43, 'ftp_data': 19, 'rje': 48, 'time': 59, 'mtp': 31, 'link': 29,
-            'remote_job': 47, 'gopher': 20, 'ssh': 52, 'name': 32, 'whois': 65, 'domain': 10,
-            'login': 30, 'imap4': 24, 'daytime': 8, 'ctf': 7, 'nntp': 38, 'shell': 49,
-            'IRC': 0, 'nnsp': 37, 'http_443': 23, 'exec': 16, 'printer': 44, 'efs': 15,
-            'courier': 5, 'uucp': 62, 'klogin': 26, 'kshell': 27, 'echo': 12, 'discard': 9,
-            'systat': 55, 'supdup': 54, 'iso_tsap': 25, 'hostnames': 21, 'csnet_ns': 6,
-            'pop_2': 42, 'sunrpc': 53, 'uucp_path': 63, 'netbios_ns': 34, 'netbios_ssn': 35,
-            'netbios_dgm': 33, 'sql_net': 51, 'vmnet': 64, 'bgp': 4, 'Z39_50': 2, 'ldap': 28,
-            'netstat': 36, 'urh_i': 60, 'X11': 1, 'urp_i': 61, 'pm_dump': 41, 'tftp_u': 57,
-            'tim_i': 58, 'red_i': 46
-        }
-
-        input_df['service'] = input_df['service'].map(service_map)
-
-        scaler = joblib.load('scalar_model.joblib')
-        num_cols = [
-            "count", "src_bytes", "dst_bytes", "dst_host_same_src_port_rate",
-            "srv_count", "dst_host_count", "dst_host_srv_diff_host_rate", "same_srv_rate"
-        ]
-        input_df[num_cols] = scaler.transform(input_df[num_cols])
-
-        return input_df
-
-    def reset_form():
-        st.session_state.reset = True
-        st.session_state.protocol = 'tcp'
-        st.session_state.service = 'http'
-        st.session_state.flag = 'SF'
-        for key in ['count', 'src_bytes', 'dst_bytes', 'dst_host_same_src_port_rate',
-                    'srv_count', 'logged_in', 'dst_host_count', 'dst_host_srv_diff_host_rate', 'same_srv_rate']:
-            st.session_state[key] = 0.00
-
-    def predict_model(input_df):
-        try:
-            prediction = model.predict(input_df)[0]
-            return "Anomaly" if prediction > 0.5 else "Normal"
-        except Exception as e:
-            return None
+    
 
     
 
